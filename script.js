c3e73d248f4ecea10cf7335181757d726962a1d2
Demir <polotovdemir7@gmail.com>
2024-07-23 11:47:12 +0300

    script.js

--- script.js
+++ script.js
@@ -25,3 +25,52 @@ console.log(allpoint);
 let bell = 90;
 const allpoin = `4. Elizar - ${bell} баллов из ${boll} `;
 console.log(allpoin);
+
+let a = 1;
+let b = 4;
+console.log(`1 x 4 = ${a * b}`);
+
+let c = 2;
+let d = 4;
+console.log(`2 x 4 = ${c * d}`);
+
+let e = 3;
+let f = 4;
+console.log(`3 x 4 = ${e * f}`);
+
+let h = 4;
+let g = 4;
+console.log(`4 x 4 = ${h * g}`);
+
+let i = 5;
+let j = 4;
+console.log(`5 x 4 = ${i * j}`);
+
+let k = 6;
+let l = 4;
+console.log(`6 x 4 = ${k * l}`);
+
+let m = 7;
+let n = 4;
+console.log(`7 x 4 = ${m * n}`);
+
+let o = 8;
+let p = 4;
+console.log(`8 x 4 = ${o * p}`);
+
+let q = 9;
+let r = 4;
+console.log(`9 x 4 = ${q * r}`);
+
+let s = 10;
+let t = 4;
+console.log(`10 x 4 = ${s * t}`)
+
+alert("Вы успешно зарегистрировались")
+
+var clean = confirm("При перезагрузке сайта, введённые данные на форме будут очищены, Вы уверены что хотите перезагрузить?");
+alert(clean);
+
+var name = prompt('Введите ваше имя');
+alert(name)
+console.log(name)
