53e58c9b091e928821e0f7d5f860ab1d8369f2e5
Demir <polotovdemir7@gmail.com>
2024-07-23 10:40:15 +0300

    first

---
+++ script.js
@@ -0,0 +1,27 @@
+let firstUserName = "Nooruz";
+let secondUserName = "Zhakshylyk";
+let thirdUserName = "Alisher";
+let fourthUserName = "Elizar";
+let firstUserPoint = 97;
+let secondUserPoint = 89;
+let thirdUserPoint = 75;
+let fourthUserPoint = 90;
+const allData = `${firstUserName} ${secondUserName} ${thirdUserName} ${fourthUserName} ${firstUserPoint} ${secondUserPoint} ${thirdUserPoint} ${fourthUserPoint}`;
+console.log(allData);
+
+let ball = 97;
+let boll = 100;
+const all = `1. Nooruz - ${ball} баллов из ${boll} `;
+console.log(all);
+
+let bull = 87;
+const allBall = `2. Zhakshylyk - ${bull} баллов из ${boll} `;
+console.log(allBall);
+
+let point = 75;
+const allpoint = `3. Alisher - ${point} баллов из ${boll} `;
+console.log(allpoint);
+
+let bell = 90;
+const allpoin = `4. Elizar - ${bell} баллов из ${boll} `;
+console.log(allpoin);
